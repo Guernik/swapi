c575b409a3644fc0f5437055dec2ac4e8bc97551
guernik <emilio.guernik@gmail.com>
2019-09-16 08:16:52 -0300

    Fix bad module name in test

--- test/utils_test.erl
+++ test/utils_test.erl
@@ -5,5 +5,5 @@
 
 escape_spaces_test() -> 
   FlattenedA = lists:flatten("escape%20spaces"),  
-  ?assertEqual(FlattenedA, lists:flatten(swapi:escape_spaces("escape spaces"))).
+  ?assertEqual(FlattenedA, lists:flatten(utils:escape_spaces("escape spaces"))).
 
